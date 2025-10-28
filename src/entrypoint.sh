63a51b0af401f395b2c041dba5f10a47e07e34df
M-chan <new.jasjan.programmer@gmail.com>
2025-10-28 23:16:15 +0500

    adding auto migrate and create super user in database

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+python3 manage.py makemigrations --no-input
+python3 manage.py migrate --no-input
+
+echo "admin123\nadmin123\n" | python3 manage.py createsuperuser --username admin --email admin@example.com
+
 python3 telegram_bot.py &
 gunicorn config.wsgi --bind 0.0.0.0:8000 --workers 4
