be6abd2f1dd1e7ace105bdc36ed066e3bd27bab2
M-chan <new.jasjan.programmer@gmail.com>
2025-10-28 21:19:11 +0500

    ci: ci configured

--- students/admin.py
+++
@@ -1,9 +0,0 @@
-from .models import Student
-from main.admin import admin_site
-from django.contrib import admin
-
-class StudentsAdmin(admin.ModelAdmin):
-    list_display = ['full_name','status','sababi','class_type']
-    search_fields = ['full_name']
-
-admin_site.register(Student, StudentsAdmin)
--- upload/urls.py
+++
@@ -1,8 +0,0 @@
-# upload/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('export_xlsx/', views.export_xlsx_to_models, name='export_xlsx_to_models'),
-    path('set_null_all/', views.set_null_all, name='set_null_all'),
-]
