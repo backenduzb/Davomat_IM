e8dfeab8b884c80fae2b2f43b2903853c8eea9d8
backenduzb <qobiljonsharipov33@gmail.com>
2025-10-30 13:00:27 +0500

    Fixing

--- src/teachers/admin.py
+++ src/teachers/admin.py
@@ -3,7 +3,7 @@ from .models import Class, ClassName
 from main.admin import admin_site
 
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ['class_teacher_full_name','class_teacher_tg_id','this_updated']
+    list_display = ['class_teacher_full_name','class_name','class_teacher_tg_id','this_updated']
     search_fields = [ 'class_teacher_full_name']
 
 class ClassNameAdmin(admin.ModelAdmin):
