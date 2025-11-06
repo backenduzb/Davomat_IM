138cd7337143f1843bdf750870b3e644a760eb08
backenduzb <qobiljonsharipov33@gmail.com>
2025-11-06 18:08:49 +0500

    Added tg xlsx database

---
+++ src/upload/management/commands/dailiy_task.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
+from students.models import Student
+from django.conf import settings
+from upload.tg_uploader import upload_document
+import os, time
+
+class Command(BaseCommand):
+    help = "Kelmagan o'quvchilarni Excel faylga eksport qiladi"
+
+    def handle(self, *args, **kwargs):
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Davomat"
+
+        ws.merge_cells("A1:E1")
+        ws["A1"].alignment = Alignment(horizontal="center", vertical="center")
+        ws["A1"] = "Kelmagan o‘quvchilar ro‘yxati"
+
+        headers = ["№", "F.I.SH", "SINF", "SANA", "SABAB"]
+        widths = [3, 60, 7, 12, 20]
+
+        for col, (header, width) in enumerate(zip(headers, widths), start=1):
+            cell = ws.cell(row=2, column=col, value=header)
+            cell.alignment = Alignment(horizontal="center", vertical="center")
+            ws.column_dimensions[chr(64 + col)].width = width
+
+        students = Student.objects.exclude(status="Bor")
+
+        row_num = 3
+        for idx, student in enumerate(students, start=1):
+            ws.cell(row=row_num, column=1, value=idx)
+            ws.cell(row=row_num, column=2, value=student.full_name)
+            ws.cell(row=row_num, column=3, value=str(student.class_type.class_name))
+            ws.cell(row=row_num, column=4, value="")
+            ws.cell(row=row_num, column=5, value=student.sababi or "-")
+            row_num += 1
+
+        file_name = os.path.join(settings.BASE_DIR, "Davomat.xlsx")
+
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        wb.save(file_name)
+
+        time.sleep(5)
+        upload_document(document=file_name)
+
+        self.stdout.write(self.style.SUCCESS(f"{len(students)} ta kelmagan o‘quvchi '{file_name}' fayliga eksport qilindi ✅"))
---
+++ src/upload/tg_uploader.py
@@ -0,0 +1,26 @@
+import requests
+from datetime import datetime
+import pytz
+
+uz_tz = pytz.timezone("Asia/Tashkent")
+now_date = datetime.now(uz_tz)
+now_caption = now_date.strftime("%Y/%m/%d")  
+
+BOT_TOKEN = "7102026834:AAFnKJiSiecmgd_giX25CzeTRGAvf1ec0Zs"
+GROUP_ID = "-1003013595617"
+
+def upload_document(document="Davomat.xlsx"):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument"
+    data = {
+        "chat_id": GROUP_ID,
+        "caption": f"📅 Sana: {now_caption}"
+    }
+
+    with open(document, "rb") as file:
+        response = requests.post(url, data=data, files={"document": file})
+
+    if response.status_code == 200:
+        print("✅ Fayl muvaffaqiyatli yuborildi!")
+    else:
+        print(f"❌ Xatolik: {response.status_code}")
+        print(response.text)
