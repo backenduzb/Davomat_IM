d7d79c616f5544036e925c1dfe99583cc9a05829
Qobiljon <qobiljonsharipov33@gmail.com>
2025-11-13 20:36:09 +0500

    Fixed scheduler bug

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -4,12 +4,12 @@ from .jobs import export_data
 
 
 def start():
-    scheduler = BackgroundScheduler(timezone=str(timezone.get_current_timezone()))
-    scheduler.add_job(
-        export_data,
-        'cron',
-        hour=19,
-        minute=0
-    )
-    scheduler.start()  
-    scheduler.start()
+    if not scheduler.running: 
+        scheduler = BackgroundScheduler(timezone=str(timezone.get_current_timezone()))
+        scheduler.add_job(
+            export_data,
+            'cron',
+            hour=19,
+            minute=0
+        )
+        scheduler.start()
