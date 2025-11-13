d9d071b8be7e4e0b2918c831fa107d329ce589e0
Qobiljon <qobiljonsharipov33@gmail.com>
2025-11-13 20:39:42 +0500

    Fixed scheduler bug

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -2,8 +2,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from django.utils import timezone
 from .jobs import export_data
 
+scheduler = BackgroundScheduler(timezone=str(timezone.get_current_timezone()))
 
 def start():
+    global scheduler
     if not scheduler.running: 
         scheduler = BackgroundScheduler(timezone=str(timezone.get_current_timezone()))
         scheduler.add_job(
