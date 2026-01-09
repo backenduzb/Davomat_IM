b280662168b1e6aff1f360375f030039b47a5b7c
Javohir <qobiljonsharipov33@gmail.com>
2026-01-09 18:16:42 +0500

    Fixing scheduler

--- src/core/apps.py
+++ src/core/apps.py
@@ -1,12 +1,12 @@
-from django.apps import AppConfig
 import os
+from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
 
     def ready(self):
-        if os.environ.get('RUN_MAIN') != 'true':
+        if os.environ.get("RUN_MAIN") != "true":
             return
 
         from .scheduler import start
