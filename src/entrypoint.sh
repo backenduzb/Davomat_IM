cdb9ceb607775e128d78e7fc40ff8bd0713f85db
Javohir <qobiljonsharipov33@gmail.com>
2026-01-11 18:31:31 +0500

    Trying use railway scheduler

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,5 +4,7 @@ python3 manage.py migrate --no-input
 python3 manage.py createadmin
 python3 manage.py collectstatic --no-input
 
+python manage.py shell -c "from core.jobs import export_data; export_data()"
+
 python3 manage.py runbot &
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4
