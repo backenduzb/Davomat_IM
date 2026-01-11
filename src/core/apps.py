077aa29575cda884cb97c0adda06821e26082d37
Javohir <qobiljonsharipov33@gmail.com>
2026-01-11 18:48:49 +0500

    Trying fix scheduler bug

--- src/core/apps.py
+++ src/core/apps.py
@@ -6,7 +6,7 @@ class CoreConfig(AppConfig):
     name = "core"
 
     def ready(self):
-        if os.environ.get("RUN_MAIN") != "true":
+        if os.environ.get("RUN_SCHEDULER") != "true":
             return
 
         from .scheduler import start
