5856d1280ce8b5ecbb402fb76ec4c044034d3f1b
Javohir <qobiljonsharipov33@gmail.com>
2026-01-25 18:10:28 +0500

    Fixing scheduler bug

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -1,11 +1,37 @@
+import fcntl
+import os
+import tempfile
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.utils import timezone
 from .jobs import export_data
 
 scheduler = BackgroundScheduler(timezone=str(timezone.get_current_timezone()))
+_lock_file = None
+
+
+def _acquire_lock():
+    global _lock_file
+
+    if _lock_file:
+        return True
+
+    lock_path = os.path.join(tempfile.gettempdir(), "davomat_apscheduler.lock")
+    lock_file = open(lock_path, "w")
+
+    try:
+        fcntl.flock(lock_file.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except BlockingIOError:
+        lock_file.close()
+        return False
+
+    _lock_file = lock_file
+    return True
 
 def start():
     global scheduler
+
+    if not _acquire_lock():
+        return
 
     if not scheduler.running:
         scheduler.start()
